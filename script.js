33646e413ff90b5e3e47f546e33b9ee03dc6e9b6
Souvik Guha <svkguha123@gmail.com>
2023-04-28 13:35:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+
+	let bkupstr = document.querySelector("#sampleTable>tbody").innerHTML;
+
+    let newstr = `<tr>
+                 <td>New Cell1</td>
+                 <td>New Cell2</td>
+                 </tr>`
+
+    document.querySelector("#sampleTable>tbody").innerHTML = newstr + bkupstr;
   
 }
